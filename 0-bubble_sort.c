bbedb2dbe68f82b010ac55897944b403d94c513e
Mark-andrew7 <kamaumark19@gmail.com>
2023-06-26 21:55:20 -0700

    fixed requirement error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,14 +6,15 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j, temp = 0;
+size_t i, j;
+int temp;
 
 if (size < 2)
 return;
 
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
+for (j = 0; j < size - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
